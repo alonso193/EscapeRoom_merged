9856b518ff5f01ec23cc9373894a834af17b1221
alonso193 <alonso.espinoza193@gmail.com>
2018-12-13 00:40:21 -0600

    including new prefabs

---
+++ Assets/Scripts/Interactive/PlayThePiano.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayThePiano : MonoBehaviour, IInteractive {
+    public string interactiveName;
+
+    private bool playState;
+    private Dictionary<string, string> carryActions;
+
+    void Start()
+    {
+        playState = false;
+    }
+
+    public string GetInteractiveName()
+    {
+        return interactiveName;
+    }
+
+    /**
+     * ====================
+     * H I T  A C T I O N S
+     * ====================
+     */
+    public Dictionary<string, string> GetHitActions(GameObject interactor, GameObject other)
+    {
+        var hitActions = new Dictionary<string, string>();
+        if (!GetComponent<AudioSource>().isPlaying)
+        {
+            hitActions["Button_X"] = "Play";
+        }
+
+        return hitActions;
+    }
+
+    public void ExecuteHitAction(string actionName, GameObject interactor, GameObject other)
+    {
+        switch (actionName)
+        {
+            case "Play":
+                PlayAction();
+                break;
+            default:
+                Debug.Log("Invalid HitAction");
+                break;
+        }
+    }
+
+    void PlayAction()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+    
+
+    /**
+     * ========================
+     * C A R R Y  A C T I O N S
+     * ========================
+     */
+    public Dictionary<string, string> GetCarryActions(GameObject interactor)
+    {
+        return carryActions;
+    }
+
+    public void ExecuteCarryAction(string actionName, GameObject interactor)
+    {
+    }
+}
---
+++ Assets/Scripts/Interactive/SafeBox/OpenCloseDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCloseDoor : MonoBehaviour {
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.enabled = true;
+        anim.SetTrigger("OpenCloseDoor");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.enabled = true;
+        anim.SetTrigger("CloseDoor");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        anim.enabled = false;
+    }
+}
